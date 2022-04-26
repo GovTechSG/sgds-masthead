122a24e36105be12fb35c55a4dae4a59b0ed334d
rav3 <rav3tey@gmail.com>
2022-04-26 17:37:43 +0800

    split into monorepo, added react support

---
+++ sgds-masthead/stencil.config.ts
@@ -0,0 +1,33 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'sgds-masthead',
+  buildEs5: 'prod',
+  globalStyle: 'src/globalStyle/global.scss',
+  plugins: [
+    sass()
+  ],
+  outputTargets: [
+    react({
+      componentCorePackage: '@govtechsg/sgds-masthead',
+      proxiesFile: '../sgds-masthead-react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
