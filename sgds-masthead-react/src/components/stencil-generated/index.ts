e4458d72afa8249177b9c26fb8bc867d51a5a4f5
YANG Lijia <yang_lijia@tech.gov.sg>
2022-06-10 14:20:55 +0800

    chore(*): bump version

--- sgds-masthead-react/src/components/stencil-generated/index.ts
+++ sgds-masthead-react/src/components/stencil-generated/index.ts
@@ -5,6 +5,7 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from '@govtechsg/sgds-masthead';
 
+import { defineCustomElements } from '@govtechsg/sgds-masthead/loader';
 
-
+defineCustomElements();
 export const SgdsMasthead = /*@__PURE__*/createReactComponent<JSX.SgdsMasthead, HTMLSgdsMastheadElement>('sgds-masthead');
